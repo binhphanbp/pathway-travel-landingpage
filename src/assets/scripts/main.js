8a049282caa90c3af1470b7389ae8099697134f3
Bình Phan <phanbinhfedev150504@gmail.com>
2025-03-23 21:26:44 +0700

    feat: Scroll to section

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,3 +1,54 @@
+// SCROLL TO SECTION
+function scrollToSection() {
+  const menuItems = document.querySelectorAll('.header .header__menu li a');
+  const heightHeader = document.querySelector('header').offsetHeight;
+
+  menuItems.forEach((item) =>
+    item.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      // Lấy giá trị của Href
+      // const attrHref = item.getAttribute('href');
+      const attrHref = event.target.getAttribute('href');
+
+      // Dùng hàm window.scrollTo
+      window.scrollTo({
+        top: document.querySelector(attrHref).offsetTop - 10,
+        behavior: 'smooth',
+      });
+
+      // Trước khi add class "active" phải remove tất cả các class "active" của item khác
+      removeClassActive();
+
+      // Add class "active" để menuItem đổi màu vàng
+      item.classList.add('active');
+    })
+  );
+  // Function remove class "active"
+  function removeClassActive() {
+    menuItems.forEach((item) => item.classList.remove('--active'));
+  }
+
+  // Active Menu khi Scroll tới Section đó
+  const sections = document.querySelectorAll('section[id]');
+  window.addEventListener('scroll', function () {
+    sections.forEach((section, index) => {
+      let scrollY = window.scrollY;
+      let sectionTop = section.offsetTop - heightHeader;
+      let sectionHeight = section.offsetHeight;
+
+      // Điều kiện để khung hình nằm trong section -> Có class "active"
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+        removeClassActive();
+        menuItems[index].classList.add('--active');
+        // menuItems.forEach((item) => item.classList.add('active'));
+      }
+    });
+  });
+}
+scrollToSection();
+// ---------------------------------------------------------------------
+
 // PROGRESSBAR
 const progressBar = () => {
   let progress = document.querySelector('.progressbar');
